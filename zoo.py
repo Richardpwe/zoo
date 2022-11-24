5e794a4419566dbca149998eab606238f8085f0b
Richardpwe <info@richardweyer.de>
2022-11-24 20:59:09 +0100

    .

---
+++ zoo.py
@@ -0,0 +1,8 @@
+class Zoo(object):
+    def __init__(self, name, strasse, hausnummer, plz, ort, eröffnungsdatum):
+        self,name = name
+        self.strasse = strasse
+        self.hausnummer = hausnummer
+        self.plz = plz
+        self.ort = ort
+        self.eröffnungsdatum = eröffnungsdatum
