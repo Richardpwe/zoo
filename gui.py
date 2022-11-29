489733c52a086be75f59e492e987731406fe4030
PeterNicos <mail@nico-peters.com>
2022-11-29 12:22:40 +0100

    Gui erweitert

--- gui.py
+++ gui.py
@@ -4,17 +4,26 @@ from PIL import Image, ImageTk
 import os
 # python -m pip install Pillow
 
+
+# Root ist das Hautpfenster
 root = tk.Tk()
 root.title("Hauptfenster von deinem Zoo")
 root.geometry("800x600")
-root.minsize(width=200, height=100)
+# root.minsize(width=200, height=100)
 # root.maxsize(width=1000, height=1000)
 # root.resizable(width=False, height=False)
 
+# man kann grundsätzliches Aussehen über Themes erreichen
+# style = ttk.Style()
+# style.theme_use("clam")
 
-# Root ist das Hautpfenster
-label1 = ttk.Label(root, text="Willkommen in deinem Zoo")
-label1.pack(side="top")
+
+label1 = ttk.Label(root, text="Willkommen in deinem Zoo", background="grey", padding=40, relief="sunken", justify="center")
+label1.pack(side="top", fill="x")
+
+# optionen fuers styling nachschauen:
+# for item in label1.keys():
+#     print(item, ": ", label1[item])
 
 label2 = ttk.Label(root, text="Hier ist ein Känguruh für dich.")
 label2.pack()
@@ -34,6 +43,42 @@ label3.pack()
 
 label2 = ttk.Label(root, text="Schön, dass du da bist!")
 label2.pack(side="top")
+
+
+# dynamische Textvariable auf Label, immer wieder set-bar mit .set-methode
+text_variable = tk.StringVar()
+text_variable.set("Label mit Dynamischer Textvariable")
+labelKlick = ttk.Label(root, textvariable=text_variable, padding=10)
+labelKlick.pack()
+
+# Eingabefeld
+def print_entry_input():
+    print(entry1.get())
+
+entry1 = ttk.Entry(root, width=30)
+entry1.pack()
+
+button_send = ttk.Button(root, text="Eingabe ausgeben", command=print_entry_input)
+button_send.pack()
+
+def delete_content():
+    entry1.delete(0, tk.END)
+    
+button_delete = ttk.Button(root, text="Clear", command=delete_content)
+button_delete.pack()
+
+# Beenden-Button
+quit_button = ttk.Button(root, text="Programm beenden", padding=5, command=root.destroy)
+quit_button.pack(side="bottom")
+
+# Button
+def say_hello():
+    print("Hallo, du hast du Button gedrückt.")
+
+button1 = ttk.Button(root, text="Klick mich", padding=5, command=say_hello)
+button1.pack(side="bottom")
+
+
 
 root.mainloop()
 
