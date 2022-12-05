5af41525b04048030719566b742d59d539b47e4a
Richardpwe <info@richardweyer.de>
2022-12-05 20:51:13 +0100

    c

--- guitest1.py
+++ guitest1.py
@@ -20,41 +20,44 @@ global mengeKangurus
 mengeKangurus = 5
 
 
+
 def say_hello():
     print("Hallo, du hast du Button gedrückt.")
 
 def kanguruAdd():
     global mengeKangurus
     mengeKangurus += 1
+    dinge_erstellen()
     print("Kanguru wurde hinzugefügt")
 
-#dinge erstellen
-button1 = ttk.Button(root, text="Klick mich", padding=5, command=say_hello)
-label = ttk.Label(text="Inside the LabelFrame")
-label1 = ttk.Label(root, text="Schön, dass du da bist!")
-labelKanguruMenge = ttk.Label(root, text = "Kangurus: " + str(mengeKangurus))
-buttonKanguruAdd = ttk.Button(root, text="Kanguru hinzufügen", padding=5, command=kanguruAdd)
+def dinge_erstellen():
+    #dinge erstellen
+    button1 = ttk.Button(root, text="Klick mich", padding=5, command=say_hello)
+    label = ttk.Label(text="Inside the LabelFrame")
+    label1 = ttk.Label(root, text="Schön, dass du da bist!")
+    labelKanguruMenge = ttk.Label(root, text = "Kangurus: " + str(mengeKangurus))
+    buttonKanguruAdd = ttk.Button(root, text="Kanguru hinzufügen", padding=5, command=kanguruAdd)
 
-image = Image.open(filename).resize((100, 100))
-photo = ImageTk.PhotoImage(image)
-
-
-# platzieren
-label1.grid(row=1,column=1)
-label.grid(row=4, column=1)
-button1.grid(row=5, column=2)
-labelKanguruMenge.grid(row=2, column=1)
-buttonKanguruAdd.grid(row=3, column=1)
-
-#Kangurus erstellen
-x=0
-while x < mengeKangurus:
-    labelname = "labelKangurubild"+ str(x)
-    #print(labelname)
-    labelname = ttk.Label(root, image=photo)
-    labelname.grid(row=2, column=x+2)
-    x += 1
+    image = Image.open(filename).resize((100, 100))
+    photo = ImageTk.PhotoImage(image)
 
 
+    # platzieren
+    label1.grid(row=1,column=1)
+    label.grid(row=4, column=1)
+    button1.grid(row=5, column=2)
+    labelKanguruMenge.grid(row=2, column=1)
+    buttonKanguruAdd.grid(row=3, column=1)
 
+    #Kangurus erstellen
+    x=0
+    while x < mengeKangurus:
+        labelname = "labelKangurubild"+ str(x)
+        #print(labelname)
+        labelname = ttk.Label(root, image=photo)
+        labelname.grid(row=2, column=x+2)
+        x += 1
+
+
+dinge_erstellen()
 root.mainloop()
