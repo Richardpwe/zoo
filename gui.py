97f7028db690c892515efc38899208916d4c3c68
PeterNicos <mail@nico-peters.com>
2022-12-05 22:41:55 +0100

    gui geupdatet aus änderungen

--- gui.py
+++ gui.py
@@ -1,118 +1,72 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-from tkinter.ttk import *
 import os
 # python -m pip install Pillow
-
 
 # Root ist das Hautpfenster
 root = tk.Tk()
 root.title("Hauptfenster von deinem Zoo")
 root.geometry("800x600")
 root.iconbitmap("favicon-zoo.ico")
-# root.minsize(width=200, height=100)
-# root.maxsize(width=1000, height=1000)
-# root.resizable(width=False, height=False)
-
-# man kann grundsätzliches Aussehen über Themes erreichen
-# style = ttk.Style()
-# style.theme_use("clam")
-
-# This will create style object
-# styleButton = Style()
- 
-# This will be adding style, and
-# naming that style variable as
-# W.Tbutton (TButton is used for ttk.Button).
-#styleButton.configure('W.TButton', font = ('calibri', 10, 'bold', 'underline'), foreground = 'red')
 
 
-label1 = ttk.Label(root, text="Willkommen in deinem Zoo", background="grey", padding=40, relief="sunken", justify="center")
-label1.pack(side="top", fill="x")
-
-# optionen fuers styling nachschauen:
-# for item in label1.keys():
-#     print(item, ": ", label1[item])
-
-label2 = ttk.Label(root, text="Hier ist ein Känguruh für dich.")
-label2.pack()
-
-
-# methode definieren
-#def kangurubild():
-    #dirname = os.path.dirname(__file__)
-    #filename = os.path.join(dirname, 'icons', 'noun-kangaroo-1866921.png')
-    #image = Image.open(filename).resize((100, 100))
-    #photo = ImageTk.PhotoImage(image)
-
-    #label3 = ttk.Label(root, image = photo)
-    #label3.pack()
-
-# Mehtode callen
-#kangurubild()
-
+#allgemeines
 dirname = os.path.dirname(__file__)
+    #kangurupfad
 filename = os.path.join(dirname, 'icons', 'noun-kangaroo-1866921.png')
-image = Image.open(filename).resize((100, 100))
-photo = ImageTk.PhotoImage(image)
 
-label3 = ttk.Label(root, image=photo)
-label3.pack(side="top")
+#kangurumenge definieren
+mengeKangurus = 0
 
-label4 = ttk.Label(root, image=photo)
-label4.pack(side="bottom")
+label_list = []
 
-
-
-# label1.configure(text="Anderer Text")
-
-
-
-label2 = ttk.Label(root, text="Schön, dass du da bist!")
-label2.pack(side="top")
-
-
-# dynamische Textvariable auf Label, immer wieder set-bar mit .set-methode
-text_variable = tk.StringVar()
-text_variable.set("Label mit Dynamischer Textvariable")
-labelKlick = ttk.Label(root, textvariable=text_variable, padding=10)
-labelKlick.pack()
-
-# Eingabefeld
-def print_entry_input():
-    print(entry1.get())
-
-entry1 = ttk.Entry(root, width=30)
-entry1.pack()
-
-button_send = ttk.Button(root, text="Eingabe ausgeben", command=print_entry_input)
-button_send.pack()
-
-def delete_content():
-    entry1.delete(0, tk.END)
-    
-button_delete = ttk.Button(root, text="Clear", command=delete_content)
-button_delete.pack()
-
-
-
-# Beenden-Button
-quit_button = ttk.Button(root, text="Programm beenden", padding=5, command=root.destroy)
-quit_button.pack(side="bottom")
-
-# Button
 def say_hello():
     print("Hallo, du hast du Button gedrückt.")
 
+def kanguruAdd():
+    global mengeKangurus
+    mengeKangurus += 1
+    label = tk.Label(root, image=photo)
+    label.grid(row=2, column=2+len(label_list))
+    label_list.append(label)
+    labelKanguruMenge["text"] =  "Kangurus: " + str(mengeKangurus)
+    print("Kanguru wurde hinzugefügt")
+
+def kanguruRemove():
+    global mengeKangurus
+    mengeKangurus -= 1
+    if label_list:
+        label_list[-1].grid_forget()
+        del label_list[-1]
+    labelKanguruMenge["text"] =  "Kangurus: " + str(mengeKangurus)
+    print("Kanguru wurde enfernt")
+
+#dinge erstellen
 button1 = ttk.Button(root, text="Klick mich", padding=5, command=say_hello)
-button1.pack(side="bottom")
+label = ttk.Label(text="Inside the LabelFrame")
+label1 = ttk.Label(root, text="Schön, dass du da bist!")
+labelKanguruMenge = ttk.Label(root, text = "Kangurus: " + str(mengeKangurus))
+buttonKanguruAdd = ttk.Button(root, text="Kanguru hinzufügen", padding=5, command=kanguruAdd)
+buttonKanguruRemove = ttk.Button(root, text="Kanguru entfernen", padding=5, command=kanguruRemove)
 
-for item in button1.keys():
-    print(item, ": ", button1[item])
+image = Image.open(filename).resize((100, 100))
+photo = ImageTk.PhotoImage(image)
 
 
+# platzieren
+label1.grid(row=1,column=1)
+label.grid(row=4, column=1)
+button1.grid(row=5, column=2)
+labelKanguruMenge.grid(row=2, column=1)
+buttonKanguruAdd.grid(row=3, column=1)
+buttonKanguruRemove.grid(row=3, column=2)
+
+for x in range(mengeKangurus):
+        labelname = "labelKangurubild"+ str(x)
+        labelname = ttk.Label(root, image=photo)
+        labelname.grid(row=2, column=x+2)
+        label_list.append(labelname)
+
+print(len(label_list))
 root.mainloop()
-
-print("Gui wurde geschlossen")
-
