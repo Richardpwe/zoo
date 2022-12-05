bfa0c86951d290c41d1a745f75bce1b9958bb508
PeterNicos <mail@nico-peters.com>
2022-12-05 20:30:38 +0100

    kaputte guitest.py ist kaputt

--- guitest1.py
+++ guitest1.py
@@ -9,13 +9,14 @@ root = tk.Tk()
 root.title("Hauptfenster von deinem Zoo")
 root.geometry("800x600")
 
+
 #allgemeines
 dirname = os.path.dirname(__file__)
     #kangurupfad
 filename = os.path.join(dirname, 'icons', 'noun-kangaroo-1866921.png')
 
     #kangurumenge definieren
-
+mengeKangurus = 5
 
 
 def say_hello():
@@ -25,21 +26,32 @@ def say_hello():
 button1 = ttk.Button(root, text="Klick mich", padding=5, command=say_hello)
 label = ttk.Label(text="Inside the LabelFrame")
 label1 = ttk.Label(root, text="Schön, dass du da bist!")
-
-
+labelKanguruMenge = ttk.Label(root, text = "Kangurus: " + str(mengeKangurus))
+buttonKanguruAdd = ttk.Button(root, text="Kanguru hinzufügen", padding=5, command=mengeKangurus+1)
 
 image = Image.open(filename).resize((100, 100))
 photo = ImageTk.PhotoImage(image)
 
-labelKangurubild1 = ttk.Label(root, image=photo)
-labelKangurubild2 = ttk.Label(root, image=photo)
 
 # platzieren
 label1.grid(row=1,column=1)
-labelKangurubild1.grid(row=2, column=1)
-labelKangurubild2.grid(row=2, column=2)
-label.grid(row=3, column=1)
-button1.grid(row=4, column=2)
+label.grid(row=4, column=1)
+button1.grid(row=5, column=2)
+labelKanguruMenge.grid(row=2, column=1)
+buttonKanguruAdd.grid(row=3, column=1)
+
+def kanguruAdd():
+    kangurumenge += 1
+    print("Kanguru wurde hinzugefügt")
+
+#Kangurus erstellen
+x=0
+while x < mengeKangurus:
+    labelname = "labelKangurubild"+ str(x)
+    #print(labelname)
+    labelname = ttk.Label(root, image=photo)
+    labelname.grid(row=2, column=x+2)
+    x += 1
 
 
 
