4a0e376339748da323a87837041fd8a8430c4d9f
Richardpwe <info@richardweyer.de>
2022-12-06 21:24:58 +0100

    j

--- gui.py
+++ gui.py
@@ -24,7 +24,6 @@ def kanguru_add(app):
         startzeile += 1
         startspalte = 1
 
-
     label = ttk.Label(root, image=app.photo)
     label.grid(row=startzeile, column=startspalte)
     startspalte += 1
@@ -55,9 +54,9 @@ class App:
         #dinge erstellen
         #label = ttk.Label(text="Inside the LabelFrame")
         #label1 = ttk.Label(root, text="Schön, dass du da bist!")
-        self.label_kanguru_menge = ttk.Label(root, text = "Kangurus: " + str(len(label_list)))
-        self.button_kanguru_add = ttk.Button(root, text="Hinzufügen", padding=5, command=lambda:kanguru_add(app))
-        self.button_kanguru_remove = ttk.Button(root, text="Entfernen", padding=5, command=kanguru_remove)
+        self.label_kanguru_menge = tk.Label(root, text = "Kangurus: " + str(len(label_list)))
+        self.button_kanguru_add = tk.Button(root, text="Hinzufügen", command=lambda:kanguru_add(app))
+        self.button_kanguru_remove = tk.Button(root, text="Entfernen", command=kanguru_remove)
 
         self.image = Image.open(konstanten.KANGURUPFAD).resize((100, 100))
         self.photo = ImageTk.PhotoImage(self.image)
@@ -71,17 +70,27 @@ class App:
 
         for i in range(konstanten.TESTKONSTANTE):
             kanguru_add(self)
+        
+        if konstanten.DARK_MODE:
+            root.config(bg='#333333')
+            self.label_kanguru_menge.config(bg='#333333')
+            self.button_kanguru_add.config(bg='#333333')
+            self.button_kanguru_remove.config(bg='#333333')
+            self.label_kanguru_menge.config(fg='#FFFFFF')
+            self.button_kanguru_add.config(fg='#FFFFFF')
+            self.button_kanguru_remove.config(fg='#FFFFFF')
 
     def set_kanguru_label(self, text):
         self.label_kanguru_menge["text"] =  "Kangurus: " + text
 
-
 if __name__ == "__main__":
     # Root ist das Hautpfenster
     root = tk.Tk()
+    #root.config(bg='#999999')
     root.title("Hauptfenster von deinem Zoo")
     root.geometry(str(konstanten.MAX_LABELS_PER_ROW)*100 + "x600")
     root.iconbitmap("favicon-zoo.ico")
+    
     app = App(root)
 
     root.mainloop()
