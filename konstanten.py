539157799188a73df5699528c6820ad5ed718de7
Richardpwe <info@richardweyer.de>
2022-12-06 22:42:43 +0100

    a

--- konstanten.py
+++ konstanten.py
@@ -2,6 +2,7 @@ import os
 MAX_LABELS_PER_ROW = 10
 TESTKONSTANTE = 60
 DARK_MODE = True
+DARK_MODE_COLOR = "#777777"
 
 #kangurupfad
 KANGURUPFAD = os.path.join(os.path.dirname(__file__), 'icons', 'noun-kangaroo-1866921.png')
