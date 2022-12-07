e85fe4b03bff02354e4026e714d575799bb09f6f
Richardpwe <info@richardweyer.de>
2022-12-07 01:18:31 +0100

    Huptmenue hinzugefuegt

---
+++ zooverwaltung.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import konstanten
+from gui import TierUebersichtFenster
+
+# Hauptfenster erstellen
+root = tk.Tk()
+#root.title("Hauptmenü")
+root.geometry("500"+"x350")
+root.iconbitmap("favicon-zoo.ico")
+root.wm_overrideredirect(True)
+
+# Funktionen für die Menüpunkte erstellen
+def open_window_1():
+    # Fenster 1 öffnen
+    TierUebersichtFenster(root)
+
+def open_window_2():
+    # Fenster 2 öffnen
+    TierUebersichtFenster(root)
+
+def open_window_3():
+    # Fenster 3 öffnen
+    TierUebersichtFenster(root)
+
+def open_window_4():
+    # Fenster 4 öffnen
+    TierUebersichtFenster(root)
+
+# Frame erstellen
+frame = tk.Frame(root)
+frame.pack(padx=20, pady=20)
+
+# Button erstellen, um Fenster 1 zu öffnen
+button_1 = tk.Button(frame, text="Fenster 1 öffnen", command=open_window_1, width=500)
+button_1.pack(pady=10)
+
+# Button erstellen, um Fenster 2 zu öffnen
+button_2 = tk.Button(frame, text="Fenster 2 öffnen", command=open_window_2, width=500)
+button_2.pack(pady=10)
+
+# Button erstellen, um Fenster 3 zu öffnen
+button_3 = tk.Button(frame, text="Fenster 3 öffnen", command=open_window_3, width=500)
+button_3.pack(pady=10)
+
+# Button erstellen, um Fenster 4 zu öffnen
+button_4 = tk.Button(frame, text="Fenster 4 öffnen", command=open_window_4, width=500)
+button_4.pack(pady=10)
+
+# Button erstellen, der das Programm beendet, wenn man darauf klickt
+quit_button = tk.Button(frame, text='Beenden', command=root.destroy, width=500)
+quit_button.pack(pady=10)
+
+if konstanten.DARK_MODE:
+    root.config(bg=konstanten.DARK_MODE_COLOR)
+    frame.config(bg=konstanten.DARK_MODE_COLOR)
+    button_1.config(bg=konstanten.DARK_MODE_COLOR)
+    button_2.config(bg=konstanten.DARK_MODE_COLOR)
+    button_3.config(bg=konstanten.DARK_MODE_COLOR)
+    button_4.config(bg=konstanten.DARK_MODE_COLOR)
+    quit_button.config(bg=konstanten.DARK_MODE_COLOR)
+
+# Hauptfenster anzeigen
+root.mainloop()
